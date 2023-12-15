b6633f20f54c99ad432bfd799a3a002872a5ec7f
Glitch (taiwa) <none>
2023-12-15 15:35:23 +0000

    🐣☂️ Checkpoint ./commands/disable.js:49372961/7636

--- commands/disable.js
+++ commands/disable.js
@@ -30,7 +30,12 @@ module.exports = {
     options: [
       {
         type: "BOOLEAN",
-        name: "超激通知channel",
+        name: "レア通知channel",
+        description: "Trueで削除、Falseで削除しない",
+      },
+      {
+        type: "BOOLEAN",
+        name: "超激レア通知channel",
         description: "Trueで削除、Falseで削除しない",
       },
       {
@@ -40,7 +45,12 @@ module.exports = {
       },
       {
         type: "BOOLEAN",
-        name: "超激通知role",
+        name: "レア通知role",
+        description: "Trueで削除、Falseで削除しない",
+      },
+      {
+        type: "BOOLEAN",
+        name: "超激レア通知role",
         description: "Trueで削除、Falseで削除しない",
       },
       {
@@ -67,29 +77,34 @@ module.exports = {
   },
   async execute(interaction) {
     if(!interaction.member.permissions.has("ADMINISTRATOR")) return interaction.reply({ content: "サーバー管理者しか使えません", ephemeral: true })
-    const ch1 = interaction.options.getBoolean("超激通知channel");
-    const ch2 = interaction.options.getBoolean("tohru枠通知channel");
-    const role1 = interaction.options.getBoolean("超激通知role");
-    const role2 = interaction.options.getBoolean("tohru枠通知role");
+    const ch1 = interaction.options.getBoolean("レア通知channel");
+    const ch2 = interaction.options.getBoolean("超激レア通知channel");
+    const ch3 = interaction.options.getBoolean("tohru枠通知channel");
+    const role1 = interaction.options.getBoolean("レア通知role");
+    const role2 = interaction.options.getBoolean("超激レア通知role");
+    const role3 = interaction.options.getBoolean("tohru枠通知role");
     const pet = interaction.options.getBoolean("ペット厳選機能");
     const percent = interaction.options.getBoolean("ペット厳選数値");
     const stop = interaction.options.getBoolean("轢き殺し防止");
     let data = await db.get(interaction.guild.id)
     if(!data){
-      data = [[undefined,undefined],[undefined,undefined],[undefined,undefined],undefined]
+      data = [[undefined,undefined,undefined],[undefined,undefined,undefined],[undefined,undefined],undefined]
       await db.set(interaction.guild.id,data)
     }
     if(ch1) data[0].splice(0,1,undefined)
     if(ch2) data[0].splice(1,1,undefined)
+    if(ch3) data[0].splice(2,1,undefined)
     if(role1) data[1].splice(0,1,undefined)
     if(role2) data[1].splice(1,1,undefined)
+    if(role3) data[1].splice(2,1,undefined)
     if(pet) data[2].splice(0,1,undefined)
     if(percent) data[2].splice(1,1,undefined)
     if(stop) data.splice(3,1,undefined)
     const embed = new MessageEmbed()
     .setTitle("設定状況:")
-    .addField(`= 超激通知用ch/role =`,`>>> ${data[0][0] != undefined ? "<#" + data[0][0] + ">" : "未設定"} / ${data[1][0] != undefined ? "<@&" + data[1][0] + ">" : "未設定"}`)
-    .addField(`= tohru枠通知用ch/role =`,`>>> ${data[0][1] != undefined ? "<#" + data[0][1] + ">" : "未設定"} / ${data[1][1] != undefined ? "<@&" + data[1][1] + ">" : "未設定"}`)
+    .addField(`= レア通知用ch/role =`,`>>> ${data[0][0] != undefined ? "<#" + data[0][0] + ">" : "未設定"} / ${data[1][0] != undefined ? "<@&" + data[1][0] + ">" : "未設定"}`)
+    .addField(`= 超激レア通知用ch/role =`,`>>> ${data[0][1] != undefined ? "<#" + data[0][1] + ">" : "未設定"} / ${data[1][1] != undefined ? "<@&" + data[1][1] + ">" : "未設定"}`)
+    .addField(`= tohru枠通知用ch/role =`,`>>> ${data[0][2] != undefined ? "<#" + data[0][2] + ">" : "未設定"} / ${data[1][2] != undefined ? "<@&" + data[1][2] + ">" : "未設定"}`)
     .addField(`= PET厳選機能 =`,`>>> ${data[2][0] != undefined ? data[2][0] : "未設定"} / ${data[2][1] != undefined ? data[2][1] + "%以上" : "未設定"}`)
     .addField(`= 轢き殺し防止 =`,`>>> ${data[3] != undefined ? data[3] : "未設定"}`)
     .setColor("RANDOM")
